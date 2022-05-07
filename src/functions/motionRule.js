01c3e4f176ee7390988aa75550215487c0c5587a
cauuagx <alexandr200550@gmail.com>
2022-05-07 14:02:27 +0300

    better govno version

--- src/functions/motionRule.js
+++ src/functions/motionRule.js
@@ -15,16 +15,15 @@ const motionRule = (motionDirection, applesPosition, setApplesPosition,
   if (motionDirection === 'w' && prevMotionDirection !== 's') {
     prevMotionDirection = motionDirection
     for (let i = 1; i <= PARSETIMES; i++) {
-      console.log(i)
-      if (i === PARSETIMES) {
-        setTimeout(() => {
+      setTimeout(() => {
+        currentPosition.unshift({
+          bottom: currentPosition[0].bottom + STEPSIZE / PARSETIMES,
+          left: currentPosition[0].left,
+          transform: 'rotate(180deg)'
+        })
+        if (i === PARSETIMES) {
           checkShakeIsDie(currentPosition)
           checkAppleIsCollect(applesPosition, currentPosition, setApplesPosition, currentScore, setCurrentScore)
-          currentPosition.unshift({
-            bottom: currentPosition[0].bottom + STEPSIZE / PARSETIMES,
-            left: currentPosition[0].left,
-            transform: 'rotate(180deg)'
-          })
           if (checkBorder(motionDirection, currentPosition)) {
             setCurrentPosition(currentPosition = currentPosition.filter((element, index) => index !== currentPosition.length - 1))
           } else {
@@ -34,31 +33,23 @@ const motionRule = (motionDirection, applesPosition, setApplesPosition,
               setCurrentPosition(currentPosition = currentPosition.filter((element, index) => index !== currentPosition.length - 1))
             }
           }
-        }, TICKRATE / PARSETIMES * i)
-      } else {
-        setTimeout(() => {
-          currentPosition.unshift({
-            bottom: currentPosition[0].bottom + STEPSIZE / PARSETIMES,
-            left: currentPosition[0].left,
-            transform: 'rotate(180deg)'
-          })
+        }else {
           setCurrentPosition(currentPosition = currentPosition.filter((element, index) => index !== currentPosition.length - 1))
-        }, TICKRATE / PARSETIMES * i)
-      }
+        }
+      }, TICKRATE / PARSETIMES * i)
     }
   } else if (motionDirection === 'a' && prevMotionDirection !== 'd') {
     prevMotionDirection = motionDirection
     for (let i = 1; i <= PARSETIMES; i++) {
-      console.log(i)
-      if (i === PARSETIMES) {
-        setTimeout(() => {
+      setTimeout(() => {
+        currentPosition.unshift({
+          bottom: currentPosition[0].bottom,
+          left: currentPosition[0].left - STEPSIZE / PARSETIMES,
+          transform: 'rotate(90deg)'
+        })
+        if (i === PARSETIMES) {
           checkShakeIsDie(currentPosition)
           checkAppleIsCollect(applesPosition, currentPosition, setApplesPosition, currentScore, setCurrentScore)
-          currentPosition.unshift({
-            bottom: currentPosition[0].bottom,
-            left: currentPosition[0].left - STEPSIZE / PARSETIMES,
-            transform: 'rotate(90deg)'
-          })
           if (checkBorder(motionDirection, currentPosition)) {
             setCurrentPosition(currentPosition = currentPosition.filter((element, index) => index !== currentPosition.length - 1))
           } else {
@@ -68,31 +59,23 @@ const motionRule = (motionDirection, applesPosition, setApplesPosition,
               setCurrentPosition(currentPosition = currentPosition.filter((element, index) => index !== currentPosition.length - 1))
             }
           }
-        }, TICKRATE / PARSETIMES * i)
-      } else {
-        setTimeout(() => {
-          currentPosition.unshift({
-            bottom: currentPosition[0].bottom,
-            left: currentPosition[0].left - STEPSIZE / PARSETIMES,
-            transform: 'rotate(90deg)'
-          })
+        }else {
           setCurrentPosition(currentPosition = currentPosition.filter((element, index) => index !== currentPosition.length - 1))
-        }, TICKRATE / PARSETIMES * i)
-      }
+        }
+      }, TICKRATE / PARSETIMES * i)
     }
   } else if (motionDirection === 'd' && prevMotionDirection !== 'a') {
     prevMotionDirection = motionDirection
     for (let i = 1; i <= PARSETIMES; i++) {
-      console.log(i)
-      if (i === PARSETIMES) {
-        setTimeout(() => {
+      setTimeout(() => {
+        currentPosition.unshift({
+          bottom: currentPosition[0].bottom,
+          left: currentPosition[0].left + STEPSIZE / PARSETIMES,
+          transform: 'rotate(270deg)'
+        })
+        if (i === PARSETIMES) {
           checkShakeIsDie(currentPosition)
           checkAppleIsCollect(applesPosition, currentPosition, setApplesPosition, currentScore, setCurrentScore)
-          currentPosition.unshift({
-            bottom: currentPosition[0].bottom,
-            left: currentPosition[0].left + STEPSIZE / PARSETIMES,
-            transform: 'rotate(270deg)'
-          })
           if (checkBorder(motionDirection, currentPosition)) {
             setCurrentPosition(currentPosition = currentPosition.filter((element, index) => index !== currentPosition.length - 1))
           } else {
@@ -102,51 +85,36 @@ const motionRule = (motionDirection, applesPosition, setApplesPosition,
               setCurrentPosition(currentPosition = currentPosition.filter((element, index) => index !== currentPosition.length - 1))
             }
           }
-        }, TICKRATE / PARSETIMES * i)
-      } else {
-        setTimeout(() => {
-          currentPosition.unshift({
-            bottom: currentPosition[0].bottom,
-            left: currentPosition[0].left + STEPSIZE / PARSETIMES,
-            transform: 'rotate(270deg)'
-          })
+        }else {
           setCurrentPosition(currentPosition = currentPosition.filter((element, index) => index !== currentPosition.length - 1))
-        }, TICKRATE / PARSETIMES * i)
-      }
+        }
+      }, TICKRATE / PARSETIMES * i)
     }
   } else if (motionDirection === 's' && prevMotionDirection !== 'w') {
     prevMotionDirection = motionDirection
     for (let i = 1; i <= PARSETIMES; i++) {
-      console.log(i)
-      if (i === PARSETIMES) {
-        setTimeout(() => {
-          checkShakeIsDie(currentPosition)
-          checkAppleIsCollect(applesPosition, currentPosition, setApplesPosition, currentScore, setCurrentScore)
-          currentPosition.unshift({
-            bottom: currentPosition[0].bottom - STEPSIZE / PARSETIMES,
-            left: currentPosition[0].left ,
-            transform: 'rotate(0deg)'
-          })
-          if (checkBorder(motionDirection, currentPosition)) {
-            setCurrentPosition(currentPosition = currentPosition.filter((element, index) => index !== currentPosition.length - 1))
-          } else {
-            if (currentPosition.length < (currentScore + 20) / 10) {
-              setCurrentPosition(currentPosition)
-            } else {
+      setTimeout(() => {
+        currentPosition.unshift({
+          bottom: currentPosition[0].bottom - STEPSIZE / PARSETIMES,
+          left: currentPosition[0].left,
+          transform: 'rotate(0deg)'
+        })
+        if (i === PARSETIMES) {
+            checkShakeIsDie(currentPosition)
+            checkAppleIsCollect(applesPosition, currentPosition, setApplesPosition, currentScore, setCurrentScore)
+            if (checkBorder(motionDirection, currentPosition)) {
               setCurrentPosition(currentPosition = currentPosition.filter((element, index) => index !== currentPosition.length - 1))
+            } else {
+              if (currentPosition.length < (currentScore + 20) / 10) {
+                setCurrentPosition(currentPosition)
+              } else {
+                setCurrentPosition(currentPosition = currentPosition.filter((element, index) => index !== currentPosition.length - 1))
+              }
             }
-          }
-        }, TICKRATE / PARSETIMES * i)
-      } else {
-        setTimeout(() => {
-          currentPosition.unshift({
-            bottom: currentPosition[0].bottom - STEPSIZE / PARSETIMES,
-            left: currentPosition[0].left ,
-            transform: 'rotate(0deg)'
-          })
+        }else {
           setCurrentPosition(currentPosition = currentPosition.filter((element, index) => index !== currentPosition.length - 1))
-        }, TICKRATE / PARSETIMES * i)
-      }
+        }
+      }, TICKRATE / PARSETIMES * i)
     }
   }
 }
